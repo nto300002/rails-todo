ee540dbdb499ce84c84f77098dbd2b00e927c075
nto300002 <samonkntd@gmail.com>
2023-04-09 13:52:48 +0900

    add: completed edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'todo_items/index'
-  get 'todo_items/create'
-  get 'todo_items/update'
-  get 'todo_items/destroy'
+  # get 'todo_items/index'
+  # get 'todo_items/create'
+  # get 'todo_items/update'
+  # get 'todo_items/destroy'
 
   resources :todo_items
   root to: 'todo_items#index'
