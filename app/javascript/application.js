2b2728de92f801ab4a40ae7f64e7fc25bb010073
nto300002 <samonkntd@gmail.com>
2023-04-09 22:11:01 +0900

    add: style

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+const logoLetters = ['C', 'h', 'a', 't', 'G', 'P', 'T', 'が作ったTODOリスト'];
+const logoColors = ['#00a3e0', '#5ac9ea', '#8dd7f6', '#0072c6', '#5ac9ea', '#00a3e0', '#8dd7f6', 'rgb(160, 160, 160)'];
+
+const logo = document.getElementById('logo');
+logo.innerHTML = logoLetters.map((letter, index) => `<span style="color:${logoColors[index]}">${letter}</span>`).join('');
