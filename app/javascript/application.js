b7e7c57f50a7f6804a88656421a7a2416c5873cf
nto300002 <samonkntd@gmail.com>
2023-04-10 19:38:48 +0900

    add: header

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,8 +2,12 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-const logoLetters = ['C', 'h', 'a', 't', 'G', 'P', 'T', 'が作ったTODOリスト'];
-const logoColors = ['#00a3e0', '#5ac9ea', '#8dd7f6', '#0072c6', '#5ac9ea', '#00a3e0', '#8dd7f6', 'rgb(160, 160, 160)'];
+// const logoLetters = ['C', 'h', 'a', 't', 'G', 'P', 'T', 'が作ったTODOリスト'];
+// const logoColors = ['#00a3e0', '#5ac9ea', '#8dd7f6', '#0072c6', '#5ac9ea', '#00a3e0', '#8dd7f6', 'rgb(160, 160, 160)'];
 
-const logo = document.getElementById('logo');
-logo.innerHTML = logoLetters.map((letter, index) => `<span style="color:${logoColors[index]}">${letter}</span>`).join('');
+// const logo = document.getElementById('logo');
+// logo.innerHTML = logoLetters.map((letter, index) => `<span style="color:${logoColors[index]}">${letter}</span>`).join('');
+
+// window.addEventListener('turbolinks:load', function() {
+//   logo.style.display = 'block';
+// });
